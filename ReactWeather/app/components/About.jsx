0632270e9e497331e05da007f62a7ee96f465609
Yoav <yoavgecht@gmail.com>
2017-08-18 10:28:32 +0300

    deleted

--- ReactWeather/app/components/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-var About = (props) => {
-    return (
-        <h3>About Component</h3>
-    )
-};
-
-export default About;
--- ReactWeather/app/components/Examples.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-var Examples = (props) => {
-    return(
-        <h3>Examples component</h3>
-    )
-}
-
-export default Examples;
